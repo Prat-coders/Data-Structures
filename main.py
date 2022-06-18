0048e463523cbc550ea40d4a941284aa23ee153a
Prat-coders <107425715+Prat-coders@users.noreply.github.com>
2022-06-18 11:54:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+list1 = [-2, -5, 5, 5, 45, 34, 78, 23, 45, 67, -34, -9, -1]
+# checking each number in the list
+for i in list1:
+     # checking for all the numbers below 0 because positive numbers fall after 0
+     if i > 0:
+      print(i)
+
+
