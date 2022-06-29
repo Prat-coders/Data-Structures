fa3b87f5b7f88d97d707ae4734cb4793ae871fe0
Prat-coders <107425715+Prat-coders@users.noreply.github.com>
2022-06-29 11:28:50 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-def most_frequent(ask):
-    num_words = {}
-    for char in ask:
-        if char not in num_words:
-            num_words[char] = 1
-        else:
-            num_words[char] += 1
-    return num_words
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+# checking each number in the list
 
+lis = []
+for numm in list2:
+    if numm >= 0:
+        lis.append(numm)
+print(f'output for list2: {lis}')
 
-print(most_frequent('mississippi'))
+for num in list1:
+    if num >= 0:
+        print(num, end=" ")
